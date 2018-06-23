f9445bb4651189af14ee95f22ba3650b2c58e6ed
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-06-23 13:25:29 +0800

    Update test sample.

--- test/test_time.py
+++ test/test_time.py
@@ -7,16 +7,17 @@ import time
 import jspcap
 
 
-lid = list()
-for index in range(1, 101):
-    now = time.time()
+for engine in {'default', 'pyshark', 'scapy', 'dpkt'}:
+    lid = list()
+    for index in range(1, 101):
+        now = time.time()
 
-    extraction = jspcap.extract(fin='../sample/in.pcap', tcp=True, store=False, nofile=True, strict=True)
+        extraction = jspcap.extract(fin='../sample/in.pcap', store=False, nofile=True, strict=True, engine=engine)
 
-    delta = time.time() - now
-    print(f'No. {index:>3d}: {extraction.length} packets extracted and reassembled in {delta} seconds.')
-    lid.append(float(delta))
+        delta = time.time() - now
+        # print(f'[{engine}] No. {index:>3d}: {extraction.length} packets extracted in {delta} seconds.')
+        lid.append(float(delta))
 
-avetime = statistics.mean(lid)
-average = avetime / extraction.length
-print(f'Report: {average} seconds per packet.')
+    avetime = statistics.mean(lid)
+    average = avetime / extraction.length
+    print(f'Report: [{engine}] {average} seconds per packet.')
