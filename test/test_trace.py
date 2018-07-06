44ec8f17d04d154e400274680ed4280044bcb42d
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-07-06 09:39:04 +0800

    New tests.

--- test/test_trace.py
+++ test/test_trace.py
@@ -7,5 +7,5 @@ import pcapkit
 
 
 trace = pcapkit.extract(fin='../sample/http.pcap', nofile=True, verbose=True,
-                       trace=True, trace_format='pcap', trace_fout='../sample/trace')
+                        trace=True, trace_format='pcap', trace_fout='../sample/trace')
 pprint.pprint(trace.trace)
