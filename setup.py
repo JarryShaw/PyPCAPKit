b7ee9ae7c47c96770fa7cf2aa1eba4453af845cb
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-11-28 17:01:30 +0800

    New distribution [0.12.10.post2]  - uploaded wheels for CPython 3.4/3.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 # version string
-__version__ = '0.12.10.post1'
+__version__ = '0.12.10.post2'
 
 # README
 with open('README.md') as file:
