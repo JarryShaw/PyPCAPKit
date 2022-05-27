24ea47e26145c4442117d9189f6b99cec4083d7a
Jarry Shaw <jarryshaw@icloud.com>
2022-05-27 15:02:31 -0700

    revised logging support * moved DEVMODE to logging * revised format & handler setup

--- pcapkit/utilities/logging.py
+++ pcapkit/utilities/logging.py
@@ -6,16 +6,38 @@ of the Python logging system, i.e. a :class:`logging.Logger`
 instance as :data:`~pcapkit.utilities.logging.logger`.
 
 """
+import os
 import logging
-
-from pcapkit.utilities.exceptions import DEVMODE
+import sys
 
 __all__ = ['logger']
+
+###############################################################################
+# Dev Mode
+###############################################################################
+
+# boolean mappings
+BOOLEAN_STATES = {'1': True, '0': False,
+                  'yes': True, 'no': False,
+                  'true': True, 'false': False,
+                  'on': True, 'off': False}
+
+#: Development mode (``DEVMODE``) flag.
+DEVMODE = BOOLEAN_STATES.get(os.environ.get('PCAPKIT_DEVMODE', 'false').casefold(), False)
+
+###############################################################################
+# Logger Setup
+###############################################################################
 
 #: logging.Logger: :class:`~logging.Logger` instance named after ``pcapkit``.
 logger = logging.getLogger('pcapkit')
 
+formatter = logging.Formatter(fmt='[%(levelname)s] %(asctime)s - %(message)s',
+                              datefmt='%m/%d/%Y %I:%M:%S %p')
+handler = logging.StreamHandler(sys.stderr)
 if DEVMODE:
+    logger.setLevel(logging.DEBUG)
+else:
     logger.setLevel(logging.INFO)
-    logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.NullHandler())
+handler.setFormatter(formatter)
+logger.addHandler(handler)
