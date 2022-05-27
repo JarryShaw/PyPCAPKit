fa80bc888168cb49f0bc7eacc621a6acfdd606ad
Jarry Shaw <jarryshaw@icloud.com>
2022-05-27 15:06:58 -0700

    revised test scripts & updated sample output files

--- test/test_extractor.py
+++ test/test_extractor.py
@@ -4,6 +4,4 @@ import pcapkit
 
 plist = pcapkit.extract(fin='../sample/in.pcap', fout='../sample/out.plist', format='plist')
 json = pcapkit.extract(fin='../sample/in.pcap', fout='../sample/out.json', format='json')
-# html = pcapkit.extract(fin='../sample/in.pcap', fout='../sample/out.js', format='html')
 tree = pcapkit.extract(fin='../sample/in.pcap', fout='../sample/out.txt', format='tree')
-# json = pcapkit.extract(fin='../sample/in.pcap', fout='../sample/out.xml', format='xml')
--- test/test_time.py
+++ test/test_time.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import multiprocessing
 import statistics
 import time
 
 import pcapkit
 from pcapkit.utilities.logging import logger
 
-#multiprocessing.freeze_support()
-logger.setLevel('CRITICAL')
+logger.setLevel('INFO')
 
-for engine in ['default', 'dpkt', 'scapy']:  #, 'pyshark', 'pipline', 'server']:
-    lid = list()
-    for index in range(1, 101):
+for engine in ['default', 'dpkt', 'scapy', 'pyshark']:
+    lid = []
+    for index in range(1):
         now = time.time()
 
-        extraction = pcapkit.extract(fin='../sample/in.pcap', store=False, nofile=True, engine=engine)
+        extraction = pcapkit.extract(fin='../sample/in.pcap', store=False, nofile=True, verbose=False, engine=engine)  # type: ignore[arg-type]
 
         delta = time.time() - now
         # print(f'[{engine}] No. {index:>3d}: {extraction.length} packets extracted in {delta} seconds.')
@@ -23,4 +21,4 @@ for engine in ['default', 'dpkt', 'scapy']:  #, 'pyshark', 'pipline', 'server']:
 
     avetime = statistics.mean(lid)
     average = avetime / extraction.length
-    print(f'Report: [{engine}] {average} seconds per packet.')
+    print(f'Report: [{engine}] {average} seconds per packet ({avetime} seconds per {extraction.length} packets).')
