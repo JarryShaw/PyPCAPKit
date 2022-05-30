386029c3b3cfeff43f77af9af3d8f5029d1da19c
Jarry Shaw <jarryshaw@icloud.com>
2022-05-29 23:37:24 -0700

    removed validators module

--- pcapkit/utilities/__init__.py
+++ pcapkit/utilities/__init__.py
@@ -13,7 +13,6 @@ several user-refined exceptions and validations.
 #from pcapkit.utilities.decorators import *
 from pcapkit.utilities.exceptions import *
 from pcapkit.utilities.logging import *
-#from pcapkit.utilities.validations import *
 from pcapkit.utilities.warnings import *
 
 __all__ = ['logger', 'warn', 'stacklevel']
