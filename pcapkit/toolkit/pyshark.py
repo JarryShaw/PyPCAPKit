e1a9265488885ce656643005a94794b935a420b3
Jarry Shaw <jarryshaw@icloud.com>
2022-05-29 19:40:18 -0700

    revised pyshark toolkit * added type anno * bugfix in methods * revised logics

--- pcapkit/toolkit/pyshark.py
+++ pcapkit/toolkit/pyshark.py
@@ -10,30 +10,37 @@ usable for its caller.
 
 """
 import ipaddress
+from typing import TYPE_CHECKING, cast
 
-from pcapkit.const.reg.linktype import LinkType as LINKTYPE
+from pcapkit.const.reg.linktype import LinkType as RegType_LinkType
+from pcapkit.foundation.traceflow import Packet as TF_Packet
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from pyshark.packet.packet import Packet
 
 __all__ = ['packet2dict', 'tcp_traceflow']
 
 
-def packet2dict(packet):
+def packet2dict(packet: 'Packet') -> 'dict[str, Any]':
     """Convert PyShark packet into :obj:`dict`.
 
     Args:
-        packet (pyshark.packet.packet.Packet): Scapy packet.
+        packet: Scapy packet.
 
     Returns:
-        Dict[str, Any]: A :obj:`dict` mapping of packet data.
+        A :obj:`dict` mapping of packet data.
 
     """
-    dict_ = dict()
+    dict_ = {}  # type: dict[str, Any]
     frame = packet.frame_info
     for field in frame.field_names:
         dict_[field] = getattr(frame, field)
 
     tempdict = dict_
     for layer in packet.layers:
-        tempdict[layer.layer_name.upper()] = dict()
+        tempdict[layer.layer_name.upper()] = {}
         tempdict = tempdict[layer.layer_name.upper()]
         for field in layer.field_names:
             tempdict[field] = getattr(layer, field)
@@ -41,7 +48,7 @@ def packet2dict(packet):
     return dict_
 
 
-def tcp_traceflow(packet):
+def tcp_traceflow(packet: 'Packet') -> 'TF_Packet | None':
     """Trace packet flow for TCP.
 
     Args:
@@ -59,20 +66,27 @@ def tcp_traceflow(packet):
         :class:`~pcapkit.foundation.traceflow.TraceFlow`
 
     """
+    if 'IP' in packet:
+        ip = cast('Packet', packet.ip)
+    elif 'IPv6' in packet:
+        ip = cast('Packet', packet.ipv6)
+    else:
+        return None
+
     if 'TCP' in packet:
-        ip = packet.ip if 'IP' in packet else packet.ipv6
-        tcp = packet.tcp
-        data = dict(
-            protocol=LINKTYPE.get(packet.layers[0].layer_name.upper()),     # data link type from global header
-            index=int(packet.number),                                       # frame number
-            frame=packet2dict(packet),                                      # extracted packet
-            syn=bool(int(tcp.flags_syn)),                                   # TCP synchronise (SYN) flag
-            fin=bool(int(tcp.flags_fin)),                                   # TCP finish (FIN) flag
-            src=ipaddress.ip_address(ip.src),                               # source IP
-            dst=ipaddress.ip_address(ip.dst),                               # destination IP
-            srcport=int(tcp.srcport),                                       # TCP source port
-            dstport=int(tcp.dstport),                                       # TCP destination port
-            timestamp=packet.frame_info.time_epoch,                         # timestamp
+        tcp = cast('Packet', packet.tcp)
+
+        data = TF_Packet(  # type: ignore[type-var]
+            protocol=RegType_LinkType.get(packet.layers[0].layer_name.upper()),  # data link type from global header
+            index=int(packet.number),                                            # frame number
+            frame=packet2dict(packet),                                           # extracted packet
+            syn=bool(int(tcp.flags_syn)),                                        # TCP synchronise (SYN) flag
+            fin=bool(int(tcp.flags_fin)),                                        # TCP finish (FIN) flag
+            src=ipaddress.ip_address(ip.src),                                    # source IP
+            dst=ipaddress.ip_address(ip.dst),                                    # destination IP
+            srcport=int(tcp.srcport),                                            # TCP source port
+            dstport=int(tcp.dstport),                                            # TCP destination port
+            timestamp=packet.frame_info.time_epoch,                              # timestamp
         )
-        return True, data
-    return False, None
+        return data
+    return None
