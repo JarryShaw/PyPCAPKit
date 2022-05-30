81a3bff425b800f3445f06aea01ab32750f69c6e
Jarry Shaw <jarryshaw@icloud.com>
2022-05-29 22:43:43 -0700

    revised init for utilities

--- pcapkit/utilities/__init__.py
+++ pcapkit/utilities/__init__.py
@@ -9,11 +9,11 @@ and :func:`~pcapkit.utilities.decorators.beholder`, and
 several user-refined exceptions and validations.
 
 """
-from pcapkit.utilities.compat import *  # pylint: disable=redefined-builtin
+#from pcapkit.utilities.compat import *  # pylint: disable=redefined-builtin
 from pcapkit.utilities.decorators import *
 from pcapkit.utilities.exceptions import *
 from pcapkit.utilities.logging import *
-from pcapkit.utilities.validations import *
+#from pcapkit.utilities.validations import *
 from pcapkit.utilities.warnings import *
 
 __all__ = ['logger', 'warn', 'stacklevel',
