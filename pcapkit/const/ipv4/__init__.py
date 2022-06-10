391ab87922eda9aa14baa86d6efb6716cc124e7b
Jarry Shaw <jarryshaw@icloud.com>
2022-06-09 18:28:14 -0700

    working on docs (pcapkit.const.ipv4)

--- pcapkit/const/ipv4/__init__.py
+++ pcapkit/const/ipv4/__init__.py
@@ -1,6 +1,42 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=unused-import
-"""IPv4 constant enumerations."""
+""":class:`~pcapkit.protocols.internet.ipv4.IPv4` Constant Enumerations
+==========================================================================
+
+This module contains all constant enumerations of
+:class:`~pcapkit.protocols.internet.ipv4.IPv4` implementations. Available
+enumerations include:
+
+.. list-table::
+   :widths: auto
+
+   * - :class:`IPv4_ClassificationLevel <pcapkit.const.ipv4.classification_level.ClassificationLevel>`
+     - Classification Level Encodings
+   * - :class:`IPv4_OptionClass <pcapkit.const.ipv4.option_class.OptionClass>`
+     - Option Classes
+   * - :class:`IPv4_OptionNumber <pcapkit.const.ipv4.option_number.OptionNumber>`
+     - IP Option Numbers [*]_
+   * - :class:`IPv4_ProtectionAuthority <pcapkit.const.ipv4.protection_authority.ProtectionAuthority>`
+     - Protection Authority Bit Assignments
+   * - :class:`IPv4_QSFunction <pcapkit.const.ipv4.qs_function.QSFunction>`
+     - QS Functions
+   * - :class:`IPv4_RouterAlert <pcapkit.const.ipv4.router_alert.RouterAlert>`
+     - IPv4 Router Alert Option Values [*]_
+   * - :class:`IPv4_ToSDelay <pcapkit.const.ipv4.tos_del.ToSDelay>`
+     - ToS (DS Field) Delay
+   * - :class:`IPv4_ToSECN <pcapkit.const.ipv4.tos_ecn.ToSECN>`
+     - ToS ECN Field
+   * - :class:`IPv4_ToSPrecedence <pcapkit.const.ipv4.tos_pre.TOSPrecedence>`
+     - ToS (DS Field) Precedence
+   * - :class:`IPv4_ToSReliability <pcapkit.const.ipv4.tos_rel.TOSReliability>`
+     - ToS (DS Field) Reliability
+   * - :class:`IPv4_ToSThroughput <pcapkit.const.ipv4.tos_thr.TOSThroughput>`
+     - ToS (DS Field) Throughput
+
+.. [*] https://www.iana.org/assignments/ip-parameters/ip-parameters.xhtml#ip-parameters-1
+.. [*] https://www.iana.org/assignments/ip-parameters/ip-parameters.xhtml#ipv4-router-alert-option-values
+
+"""
 
 from pcapkit.const.ipv4.classification_level import ClassificationLevel as IPv4_ClassificationLevel
 from pcapkit.const.ipv4.option_class import OptionClass as IPv4_OptionClass
