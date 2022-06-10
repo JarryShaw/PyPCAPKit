940fa9d36f4c9655f4a81331fdb8b0cfcf48ff55
Jarry Shaw <jarryshaw@icloud.com>
2022-06-09 18:06:49 -0700

    working on docs (pcapkit.const.http

--- pcapkit/const/http/__init__.py
+++ pcapkit/const/http/__init__.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=unused-import
-"""HTTP constant enumerations."""
+""":class:`~pcapkit.protocols.application.http.HTTP` Constant Enumerations
+=============================================================================
+
+This module contains all constant enumerations of
+:class:`~pcapkit.protocols.application.http.HTTP` implementations. Available
+enumerations include:
+
+.. list-table::
+   :widths: auto
+
+   * - :class:`HTTP_ErrorCode <pcapkit.const.http.error_code.ErrorCode>`
+     - HTTP/2 Error Code [*]_
+   * - :class:`HTTP_Frame <pcapkit.const.http.frame.Frame>`
+     - HTTP/2 Frame Type [*]_
+   * - :class:`HTTP_Setting <pcapkit.const.http.setting.Setting>`
+     - HTTP/2 Settings [*]_
+
+.. [*] https://www.iana.org/assignments/http2-parameters/http2-parameters.xhtml#error-code
+.. [*] https://www.iana.org/assignments/http2-parameters/http2-parameters.xhtml#frame-type
+.. [*] https://www.iana.org/assignments/http2-parameters/http2-parameters.xhtml#settings
+
+"""
 
 from pcapkit.const.http.error_code import ErrorCode as HTTP_ErrorCode
 from pcapkit.const.http.frame import Frame as HTTP_Frame
