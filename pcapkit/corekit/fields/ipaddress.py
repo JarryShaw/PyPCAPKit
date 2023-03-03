64d7bca2d1857e3ba2218446a0591005067ea10f
Jarry Shaw <jarryshaw@icloud.com>
2023-03-02 17:33:21 -0800

    make isort

--- pcapkit/corekit/fields/ipaddress.py
+++ pcapkit/corekit/fields/ipaddress.py
@@ -14,7 +14,7 @@ __all__ = [
 
 if TYPE_CHECKING:
     from ipaddress import IPv4Address, IPv6Address
-    from typing import Callable, Any
+    from typing import Any, Callable
 
     from typing_extensions import Literal
 
