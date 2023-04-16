a26680988fb98faac65aa4b8d86299329d5980ae
Jarry Shaw <jarryshaw@icloud.com>
2023-04-16 09:04:56 +0800

    working on PCAPNG schema

---
+++ pcapkit/protocols/schema/misc/pcapng.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# mypy: disable-error-code=assignment
+"""header schema for pcapng file format"""
+
+from typing import TYPE_CHECKING
+
+from pcapkit.const.pcapng.block_type import BlockType as Enum_BlockType
+from pcapkit.corekit.fields.numbers import UInt8Field, UInt16Field, UInt32Field, UInt64Field, EnumField
+from pcapkit.corekit.fields.strings import BytesField, PaddingField, BitField
+from pcapkit.corekit.fields.misc import PayloadField
+from pcapkit.protocols.schema.schema import Schema
+
+__all__ = [
+    'PCAPNG',
+
+    'UnknownBlock',
+]
+
+
+class PCAPNG(Schema):
+    """Header schema for PCAP-NG file blocks."""
+
+    #: Block type.
+    type: 'Enum_BlockType' = EnumField(length=4, namespace=Enum_BlockType)
+    #: Block total length.
+    length: 'int' = UInt32Field()
+
+
+class UnknownBlock(PCAPNG):
+    """Header schema for unknown PCAP-NG file blocks."""
+
+    #: Block body (including padding).
+    body: 'bytes' = PayloadField(length=lambda pkt: pkt['length'])
+    #: Block total length.
+    length2: 'int' = UInt32Field()
+
+    def __init__(self, type: 'Enum_BlockType', length: 'int', body: 'bytes', length2: 'int') -> 'None': ...
