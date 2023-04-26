b23cf6a5925b39d811d8d2341bf27ca32e33c8ea
Jarry Shaw <jarryshaw@icloud.com>
2023-04-26 16:01:54 +0800

    updated Data typing info

--- pcapkit/protocols/data/data.py
+++ pcapkit/protocols/data/data.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 """base class for data models"""
 
+from typing import TYPE_CHECKING
+
 from pcapkit.corekit.infoclass import Info
 
 __all__ = ['Data']
+
+if TYPE_CHECKING:
+    from typing import Type
+
+    from pcapkit.protocols.protocol import Protocol
 
 
 class Data(Info):
     """Base class for data models."""
 
     __excluded__ = ['__next_name__', '__next_type__']
+
+    if TYPE_CHECKING:
+        #: Next field name, i.e., the name of the payload field.
+        __next_name__: 'str'
+        #: Next field type, i.e., the type of the payload field.
+        __next_type__: 'Type[Protocol]'
