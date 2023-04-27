9b992910447a0bf3459576055b298564c3e2b3c5
JarryShaw <jarryshaw@icloud.com>
2023-04-27 04:21:29 +0000

    Deploying to gh-pages from @ JarryShaw/PyPCAPKit@bca119c333c2a83e937225dc532b2befa99d997d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0b20',
+    VERSION: '1.0.0b21',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
