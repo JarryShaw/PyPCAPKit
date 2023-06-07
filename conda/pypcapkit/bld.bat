753a8590849e8490a1279403c6137a69e119e974
Jarry Shaw <jarryshaw@icloud.com>
2023-06-07 17:20:45 +0800

    debugging conda workflow (windows)

--- conda/pypcapkit/bld.bat
+++ conda/pypcapkit/bld.bat
@@ -1,3 +1,10 @@
-"%PYTHON%" -m pip install conda\wheels\* --target pcapkit\_extern -vv
+@ECHO OFF
+
+REM "%PYTHON%" -m pip install conda\wheels\* --target pcapkit/_extern -vv
+FOR /F "Tokens=*" %A IN ('DIR /B conda\wheels\') DO @(
+    "%PYTHON%" -m pip install conda\wheels\%A --target pcapkit\_extern -vv
+)
+
 "%PYTHON%" -m pip install . -vv
-if errorlevel 1 exit 1
+
+IF ERRORLEVEL 1 EXIT 1
