e750ed3326cdf4740ffae5e2b3bec487621b0880
Jarry Shaw <jarryshaw@icloud.com>
2023-06-07 16:06:49 +0800

    debugging conda workflow

--- conda/pypcapkit/bld.bat
+++ conda/pypcapkit/bld.bat
@@ -1,3 +1,3 @@
-"%PYTHON%" -m pip install conda/wheels/* --target pcapkit/_extern -vv
+"%PYTHON%" -m pip install conda\wheels\* --target pcapkit\_extern -vv
 "%PYTHON%" -m pip install . -vv
 if errorlevel 1 exit 1
