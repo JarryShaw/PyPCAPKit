a21fb25dbf0fcad3989d6c451264c219496046ff
Jarry Shaw <jarryshaw@icloud.com>
2023-06-28 11:43:00 +0800

    added markers to conda req

--- util/conda-dist.py
+++ util/conda-dist.py
@@ -6,13 +6,16 @@ import pkg_resources
 
 req_file = os.path.join('conda', 'requirements.txt')
 
-data = {}  # dict[str, str]
+data = {}  # dict[str, tuple[str, str]]
 with open(req_file) as file:
     req_map = pkg_resources.parse_requirements(file)
     for req in req_map:
         dist = pkg_resources.get_distribution(req.key)
-        data[req.key] = dist.version
+        data[req.key] = (dist.version, req.marker)
 
 with open(req_file, 'w') as file:
-    for key, value in sorted(data.items()):
-        print(f'{key}=={value}', file=file)
+    for key, (version, marker) in sorted(data.items()):
+        if marker is None:
+            print(f'{key} == {version}', file=file)
+        else:
+            print(f'{key} == {version} ; {marker}', file=file)
