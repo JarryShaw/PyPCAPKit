fea58337a05b487bf2b4e320fd72405f74ced738
Jarry Shaw <jarryshaw@icloud.com>
2023-08-26 19:24:53 +0800

    revised test cases

--- test/test_stream_askpass.py
+++ test/test_stream_askpass.py
@@ -5,7 +5,7 @@ import subprocess  # nosec: B404
 
 import pcapkit
 
-with subprocess.Popen(shlex.split('sudo tcpdump -i en0 -s 0 -w - -U'),  # nosec: B603
+with subprocess.Popen(shlex.split('sudo -A tcpdump -i en0 -s 0 -w - -U'),  # nosec: B603
                       stdout=subprocess.PIPE) as file:
     pcapkit.extract(fin=file.stdout, fout='../sample/stream.txt', format='tree', no_eof=True,
                     verbose=True, buffer_save=True, buffer_path='../sample/stream.pcap')
