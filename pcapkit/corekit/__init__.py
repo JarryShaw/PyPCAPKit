0ad8fb303a6fb305593a144fbf1eb1c23be8699d
Jarry Shaw <jarryshaw@icloud.com>
2023-10-03 11:23:40 -0400

    implemented ModuleDescriptor namedtuple for holding import info

--- pcapkit/corekit/__init__.py
+++ pcapkit/corekit/__init__.py
@@ -12,12 +12,14 @@ class :class:`~pcapkit.corekit.infoclass.Info`,
 protocol collection class :class:`~pcapkit.corekit.protochain.ProtoChain`,
 and :class:`~pcapkit.corekit.multidict.MultiDict` family inspired from
 :mod:`Werkzeug` for multientry :obj:`dict` data mapping, the
-:class:`~pcapkit.corekit.fields.field.Field` family for data parsing.
+:class:`~pcapkit.corekit.fields.field.Field` family for data parsing,
+etc.
 
 """
 from pcapkit.corekit.io import SeekableReader
 from pcapkit.corekit.fields import *
 from pcapkit.corekit.infoclass import Info, info_final
+from pcapkit.corekit.module import ModuleDescriptor
 from pcapkit.corekit.multidict import MultiDict, OrderedMultiDict
 from pcapkit.corekit.protochain import ProtoChain
 from pcapkit.corekit.version import VersionInfo
@@ -52,4 +54,6 @@ __all__ = [
     'IPv4InterfaceField', 'IPv6InterfaceField',
 
     'SeekableReader',
+
+    'ModuleDescriptor',
 ]
---
+++ pcapkit/corekit/module.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Module Descriptor
+=======================
+
+.. module:: pcapkit.corekit.module
+
+:mod:`pcapkit.corekit.module` contains :obj:`tuple`
+like class :class:`~pcapkit.corekit.module.ModuleDescriptor`,
+which is originally designed as :obj:`tuple[str, str] <tuple>`.
+
+"""
+import collections
+import importlib
+from typing import Generic, TypeVar, TYPE_CHECKING
+
+__all__ = ['ModuleDescriptor']
+
+if TYPE_CHECKING:
+    from typing import Type
+
+T = TypeVar('T')
+
+
+class ModuleDescriptor(collections.namedtuple('ModuleDescriptor', ['module', 'name']), Generic[T]):
+    """Module descriptor contains module name and class name, the actual
+    class can be imported by ``from module import name``."""
+
+    __slots__ = ()
+
+    #: Module name.
+    module: str
+    #: Class name.
+    name: str
+
+    @property
+    def klass(self) -> 'Type[T]':
+        """Import class from module."""
+        module = importlib.import_module(self.module)
+        return getattr(module, self.name)
